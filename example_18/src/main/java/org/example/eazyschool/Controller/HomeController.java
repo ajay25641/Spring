3f5114665b6b09ae3733b4149da706d0cc1d5f6f
ajay25641 <ajay25641@gmail.com>
2023-03-19 18:21:17 +0530

    at lecture 78

--- example_18/src/main/java/org/example/eazyschool/Controller/HomeController.java
+++ example_18/src/main/java/org/example/eazyschool/Controller/HomeController.java
@@ -1,12 +1,14 @@
 package org.example.eazyschool.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
     @RequestMapping(value={"","/home","/"})
-    public String displayHomePage(){
+    public String displayHomePage(Model model){
+        model.addAttribute("username","ajay kumar");
         return "home.html";
     }
 }
