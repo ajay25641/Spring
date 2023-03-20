7c8ef8dd35109bc44323028d6e7cf90688146b12
ajay25641 <ajay25641@gmail.com>
2023-03-20 20:22:28 +0530

    at lecture 84

---
+++ example_18/src/main/java/org/example/eazyschool/Controller/ContactController.java
@@ -0,0 +1,12 @@
+package org.example.eazyschool.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ContactController {
+    @RequestMapping(value={"/contact"})
+    public String displayContactPage(){
+        return "contact.html";
+    }
+}
