a244ffba4d8015eab9d2d1f35b77f086abd8c205
ajay kumar <ajay25641@gmail.com>
2023-03-21 15:35:17 +0530

    at lecture 84

--- example_18/src/main/java/org/example/eazyschool/Controller/ContactController.java
+++ example_18/src/main/java/org/example/eazyschool/Controller/ContactController.java
@@ -1,12 +1,23 @@
 package org.example.eazyschool.Controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.logging.Logger;
+
 @Controller
 public class ContactController {
+    private Logger logger=LoggerFactory.getLogger(ContactController.class);
     @RequestMapping(value={"/contact"})
     public String displayContactPage(){
         return "contact.html";
     }
-}
+
+    @RequestMapping(value={"/saveMsg"})
+        public String saveContactDetails(){
+            return "";
+        }
+    }
+
+
