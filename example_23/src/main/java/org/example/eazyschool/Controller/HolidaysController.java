28206fc94b3f9d71a29fa80362886a6f13d19507
ajay25641 <ajay25641@gmail.com>
2023-03-27 05:51:33 +0530

    at lecture 105

--- example_23/src/main/java/org/example/eazyschool/Controller/HolidaysController.java
+++ example_23/src/main/java/org/example/eazyschool/Controller/HolidaysController.java
@@ -6,7 +6,10 @@ import org.example.eazyschool.Model.Holiday;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.websocket.server.PathParam;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,9 +19,9 @@ import java.util.stream.Collectors;
 @Controller
 public class HolidaysController {
 
-    @GetMapping(value="/holidays")
+    @GetMapping(value="/holidays/{display}")
 
-    public String getHolidays(@RequestParam(required=false) boolean festival,@RequestParam(required=false) boolean federal,Model model){
+    public String getHolidays(@PathVariable String display,Model model){
         List<Holiday> holidays= Arrays.asList(new Holiday(" Jan 1 ","New's year's Day", Holiday.Type.FESTIVAL),
                                               new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
                                               new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
@@ -29,9 +32,20 @@ public class HolidaysController {
                                               new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
         );
         Holiday.Type[] types=Holiday.Type.values();
-        model.addAttribute("festival",festival);
-        model.addAttribute("federal",federal);
-
+        //model.addAttribute("festival",festival);
+        //model.addAttribute("federal",federal);
+        boolean festival=false;
+        boolean federal=false;
+        if(display!=null){
+            if(display.equals("all")){
+                festival=true;
+                federal=true;
+            }
+            else if(display.equals("festival")) festival=true;
+            else if(display.equals("federal")) federal=true;
+            model.addAttribute("festival", festival);
+            model.addAttribute("federal", federal);
+        }
 
         for(Holiday.Type type :types){
             model.addAttribute(type.toString(),(holidays.stream().filter(holiday-> holiday.getType().equals(type)).collect(Collectors.toList())));
